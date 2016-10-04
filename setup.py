da6e17e468bd93197183cad16a01cb6f233f7344
Chris Calderon <calderon.christian760@gmail.com>
2016-10-04 13:03:02 -0700

    Refactored setup.py, finished script installer

--- setup.py
+++ setup.py
@@ -1,34 +1,39 @@
-
 from __future__ import print_function
 from setuptools import setup
 import os
-import sys
-
-if sys.version_info >= (3, 0):
-    from functools import reduce
+import json
 
 
 class SetupError(Exception):
     pass
 
 
-def is_http(req):
-    """Checks if a requirement is a link."""
-    return req.startswith('http://') or req.startswith('https://')
+def is_http(line):
+    """Checks if a requirement.txt line is a link."""
+    return line.startswith('http://') or line.startswith('https://')
 
 
-def split_requirements(links_requirements, req):
-    """Scans through requirements.txt, extracting package info from links."""
-    links, requirements = links_requirements
-    if is_http(req):
-        i = req.find('#egg')
-        if i == -1:
-            raise SetupError('Missing \'#egg=\' in requirement link.')
-        links.append(req[:i])
-        requirements.append(req[i+5:])
-    else:
-        requirements.append(req)
-    return links, requirements
+def get_requirement_info():
+    """Extracts dependency links and package names from requirements.txt"""
+    links, requirements = [], []
+    info = {'dependency_links': links, 'install_requires': requirements}
+    requirements_path = 'requirements.txt'
+
+    if not os.path.isfile(requirements_path):
+        print('requirements.txt not found. Did you forget it?')
+        return info
+
+    reqs = filter(None, map(str.strip, open(requirements_path)))
+    for line in reqs:
+        if is_http(line):
+            i = line.find('#egg=')
+            if i == -1:
+                raise SetupError('Missing \'#egg=\' in requirement link.')
+            links.append(line[:i])
+            requirements.append(line[i+5:])
+        else:
+            requirements.append(line)
+    return info
 
 
 def read_metadata():
@@ -40,7 +45,7 @@ def read_metadata():
             print('Found package:', subdir)
             break
     else:
-        raise SetupError('Can\'t find an __init__.py file!')
+        raise SetupError('No package found! Did you forget an __init__.py?')
 
     metadata = {'name': subdir, 'packages': [subdir]}
     relevant_keys = {'__version__': 'version',
@@ -48,25 +53,24 @@ def read_metadata():
                      '__email__': 'author_email',
                      '__license__': 'license'}
 
-    with open(os.path.join(subdir, '__init__.py')) as m:
-        first_line = next(m)
-        metadata['description'] = first_line.strip(). strip('\n "')
-        for line in m:
-            if len(relevant_keys) == 0:
+    m = open(os.path.join(subdir), '__init__.py')
+    first_line = next(m)
+    metadata['description'] = first_line.strip(). strip('\n "')
+    for line in m:
+        if len(relevant_keys) == 0:
+            break
+        for key in relevant_keys:
+            if line.startswith(key):
                 break
-            for key in relevant_keys:
-                if line.startswith(key):
-                    break
-            else:
-                continue
+        else:
+            continue
 
-            metadatum_name = relevant_keys.pop(key)
-            metadata[metadatum_name] = line.split('=', 1)[1].strip('\n\'\" ')
+        metadatum_name = relevant_keys.pop(key)
+        metadata[metadatum_name] = line.split('=', 1)[1].strip('\n\'\" ')
 
     if relevant_keys:
         print('FYI; You didn\'t put the following info in your __init__.py:')
         print('   ', ', '.join(relevant_keys))
-
     return metadata
 
 
@@ -74,26 +78,21 @@ def locate_scripts():
     """Finds scripts in the package."""
     scripts = []
     bin_dir = os.path.join(os.getcwd(), 'bin')
-    if os.path.isdir(bin_dir):
-        for item in os.listdir(bin_dir):
-            full_path = os.path.join(bin_dir, item)
-            if os.path.isfile(full_path):
-                with open(full_path) as f:
-                    first_line = next(f)
-                    if first_line.startswith('#!'):
-                        scripts.append(full_path)
+    if not os.path.isdir(bin_dir):
+        return scripts
+    for item in os.listdir(bin_dir):
+        full_path = os.path.join(bin_dir, item)
+        if os.path.isfile(full_path):
+            with open(full_path) as f:
+                first_line = next(f)
+            if first_line.startswith('#!'):
+                    scripts.append(full_path)
     return scripts
 
-
-with open('requirements.txt') as reqs:
-    links, requirements = reduce(split_requirements,
-                                 filter(None, map(str.strip, reqs)),
-                                 [[], []])
-
 metadata = read_metadata()
-metadata['dependency_links'] = links
-metadata['install_requires'] = requirements
+metadata.update(get_requirement_info())
 metadata['scripts'] = locate_scripts()
-print(metadata)
+
+print(json.dumps(metadata, indent=2, sort_keys=True))
 
 setup(**metadata)
